2cdb36b8884518cf5aab5eb04067a27cccfd4ddf
pacholoamit <pacholoamit.tech@gmail.com>
2022-11-18 09:13:18 +0800

    minor refactor for connection options

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 import { ConnectionOptions } from "jsforce";
 import createApp from "@/app";
 
+const options: ConnectionOptions = {
+  oauth2: {
+    clientId: process.env.SALESFORCE_CLIENT_ID,
+    clientSecret: process.env.SALESFORCE_CLIENT_SECRET,
+    redirectUri: process.env.SALESFORCE_REDIRECT_URI,
+  },
+  accessToken: process.env.SALESFORCE_ACCESS_TOKEN,
+  refreshToken: process.env.SALESFORCE_REFRESH_TOKEN,
+  instanceUrl: process.env.SALESFORCE_INSTANCE_URL,
+  version: "56.0",
+};
+
 const startApp = async () => {
-  const options: ConnectionOptions = {
-    oauth2: {
-      clientId: process.env.SALESFORCE_CLIENT_ID,
-      clientSecret: process.env.SALESFORCE_CLIENT_SECRET,
-      redirectUri: process.env.SALESFORCE_REDIRECT_URI,
-    },
-    accessToken: process.env.SALESFORCE_ACCESS_TOKEN,
-    refreshToken: process.env.SALESFORCE_REFRESH_TOKEN,
-    instanceUrl: process.env.SALESFORCE_INSTANCE_URL,
-    version: "56.0",
-  };
-
   const app = createApp(options);
-
   await app.run();
 };
 
