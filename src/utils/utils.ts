ce2bef7b0065b4b2a8af4c87895e98ff1a8f42f7
pacholoamit <pacholoamit.tech@gmail.com>
2022-11-19 06:56:59 +0800

    Fix match func

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,9 +10,10 @@ export const match = <T extends {}>(
   obj: T,
   criteria: { [key in keyof Partial<T>]: string }
 ): boolean => {
-  return Object.keys(obj).some((key) => {
+  return Object.keys(criteria).some((key) => {
     if (typeof key === "string") {
       const k = obj[key] as string;
+
       return k.includes(criteria[key]);
     }
     return obj[key] === criteria[key];
