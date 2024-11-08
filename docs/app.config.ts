6e195943e539177586e35e9cb810dee0697f90b6
pacholoamit <pacholoamit.tech@gmail.com>
2024-11-09 07:50:29 +0800

    add docus

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,39 +1,53 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: "Pachtop",
+    description: `Pachtop - a more modern system monitor. Pachtop is a lightweight, performant, and opensource system monitor that provides real-time monitoring of your system's performance, application monitoring, and detailed system information`,
+    url: "https://pachtop.com",
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      github: "pacholoamit/pachtop",
+      youtube: "@pacholoamit",
+      linkedin: {
+        label: "Pacholo Amit",
+        icon: "fa6-brands:linkedin-in",
+        href: "https://www.linkedin.com/in/pacholo-amit/",
+      },
     },
     github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      dir: ".starters/default/content",
+      branch: "main",
+      repo: "docus",
+      owner: "nuxt-themes",
+      edit: false,
     },
     aside: {
       level: 0,
       collapsed: false,
-      exclude: []
+      exclude: [],
     },
     main: {
-      padded: true,
-      fluid: true
+      padded: false,
+      fluid: true,
+    },
+    footer: {
+      credits: false,
+      textLinks: [
+        {
+          text: "Built with ❤️ by Pacholo Amit",
+          href: "https://www.pacholoamit.com",
+          rel: "noopener",
+          target: "_blank",
+        },
+      ],
     },
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
-    }
-  }
-})
+      fluid: true,
+    },
+  },
+  umami: {
+    version: 2,
+  },
+});
