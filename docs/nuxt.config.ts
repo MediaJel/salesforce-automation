d493f8c2fc54e63da8d7f53351091a7b2325901e
pacholoamit <pacholoamit.tech@gmail.com>
2024-11-09 08:40:13 +0800

    style: Remove extra white space in nuxt.config.ts

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ["@nuxt-themes/docus", "nuxt-umami"],
   devtools: { enabled: true },
-  
+
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
