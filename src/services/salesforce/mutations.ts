066716277b1d66a5ea00b1e12d0b75e9d5f6cabd
pacholoamit <pacholoamit.tech@gmail.com>
2024-11-20 13:07:03 +0800

    feat: Add logic to update Salesforce account with Quickbooks Id

--- src/services/salesforce/mutations.ts
+++ src/services/salesforce/mutations.ts
@@ -1,8 +1,13 @@
-import { Connection } from 'jsforce';
+import { Connection } from "jsforce";
 
-import { Logger } from '@/utils/types';
+import { Logger } from "@/utils/types";
 
 interface SalesforceUpdateOpportunityInput {
+  Id: string;
+  [key: string]: any;
+}
+
+interface SalesforceUpdateAccountInput {
   Id: string;
   [key: string]: any;
 }
@@ -14,6 +19,18 @@ const createSalesforceMutations = (client: Connection, logger: Logger) => {
         client.sobject("Opportunity").update(input, (err, result) => {
           if (err) {
             logger.error({ message: "Error updating Opportunity", err });
+            reject(err);
+          }
+
+          resolve(result);
+        });
+      });
+    },
+    updateAccount: async (input: SalesforceUpdateAccountInput) => {
+      return new Promise((resolve, reject) => {
+        client.sobject("Account").update(input, (err, result) => {
+          if (err) {
+            logger.error({ message: "Error updating Account", err });
             reject(err);
           }
 
